89d19c6dbadec6868069d0881031debebba89b88
AndrewGustafson <85640582+AndrewGustafson@users.noreply.github.com>
2023-11-16 11:41:23 -0500

    Update Dark_Photon_Rate.py
    Added code to make exclusion for epsilon vs mA

--- Dark_Photon_Rate.py
+++ Dark_Photon_Rate.py
@@ -32,6 +32,9 @@ mchiGeV = mchi * 1e-6
 mchiMeV = mchi * 1e-3
 mAMeV = mA * 1e-6
 
+
+#Determine Rate for Single Set of parameters
+'''
 Phi = DMMod.FluxDiff(f0, sigmav, mchiGeV) #cm^{-2} s^{-1}
 
 eps2eff = epsilon**2 * DPMod.Eps2Scaling(element,density,mchiMeV,mAMeV)
@@ -42,7 +45,33 @@ Rate = eps2eff * Volume * Phi * (PiTim/mchiMeV) * (100)**3 * MeV_to_inv_cm #s^{-
 
 print(Rate/Volume, "s^{-1} m^{-3}")
 print(Rate*3.1e7/mass, "yr^{-1} ton^{-1}")
+'''
 
+#Determine Exclusions Given a Desired Rate
+'''
+Rate_Des = 2 *mass /(3.1e7) #2 events per ton per year
 
+mA_vals = np.logspace(-2,2,300) #eV
+eps_true_vals = np.array([])
 
+for mA in mA_vals:
+    print('mA', mA)
+    mAMeV = mA * 1e-6
+    
+    Phi = DMMod.FluxDiff(f0, sigmav, mchiGeV) #cm^{-2} s^{-1}
 
+    eps2eff = epsilon**2 * DPMod.Eps2Scaling(element,density,mchiMeV,mAMeV)
+
+    PiTre,PiTim = DPMod.PiT(element, density, mchiMeV) # MeV^2
+
+    Rate = eps2eff * Volume * Phi * (PiTim/mchiMeV) * (100)**3 * MeV_to_inv_cm #s^{-1}
+    
+    eps_des = np.sqrt(Rate_Des/Rate) * epsilon
+    
+    eps_true_vals = np.append(eps_true_vals,eps_des)
+    
+fig = plt.figure()
+plt.plot(mA_vals, eps_true_vals)
+plt.xscale('log')
+plt.yscale('log')
+'''
